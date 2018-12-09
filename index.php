46af6800f8ba44200485427a672fd8ebb34a7c3b
Maximilian Groß <maximilian@elektro-gross.at>
2018-12-09 14:31:26 +0100

    QrCode

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once "vendor/autoload.php";
+
+/*use Endroid\QrCode\QrCode;
+
+$qrCode = new QrCode('Life is too short to be generating QR codes');
+
+header('Content-Type: '.$qrCode->getContentType());
+echo $qrCode->writeString();
+*/
+
+use Endroid\QrCode\ErrorCorrectionLevel;
+use Endroid\QrCode\LabelAlignment;
+use Endroid\QrCode\QrCode;
+use Endroid\QrCode\Response\QrCodeResponse;
+
+// Create a basic QR code
+$qrCode = new QrCode('Life is too short to be generating QR codes');
+$qrCode->setSize(300);
+
+// Set advanced options
+$qrCode->setWriterByName('png');
+$qrCode->setMargin(10);
+$qrCode->setEncoding('UTF-8');
+$qrCode->setErrorCorrectionLevel(new ErrorCorrectionLevel(ErrorCorrectionLevel::HIGH));
+$qrCode->setForegroundColor(['r' => 0, 'g' => 0, 'b' => 0, 'a' => 0]);
+$qrCode->setBackgroundColor(['r' => 255, 'g' => 255, 'b' => 255, 'a' => 0]);
+$qrCode->setRoundBlockSize(true);
+$qrCode->setValidateResult(false);
+$qrCode->setWriterOptions(['exclude_xml_declaration' => true]);
+
+// Directly output the QR code
+header('Content-Type: '.$qrCode->getContentType());
+echo $qrCode->writeString();
+
+// Save it to a file
+$qrCode->writeFile(__DIR__.'/qrcode.png');
+
+// Create a response object
+$response = new QrCodeResponse($qrCode);
+
+?>
